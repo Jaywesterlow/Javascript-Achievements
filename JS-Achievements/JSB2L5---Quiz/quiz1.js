b0f0cded1e11bf82484752a88b73c49b7326968f
Jaymar Westerlow <jaywesterlow71@gmail.com>
2020-12-18 14:04:28 +0100

    Healthbar & Quiz

---
+++ JS-Achievements/JSB2L5---Quiz/quiz1.js
@@ -0,0 +1,61 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/03/LAM-Lisse-logo-white.svg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Hou je van fruit",
+				"answers": [{
+						"answer": "Ja",
+						"feedback": true
+					},
+					{
+						"answer": "Nee",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welk van de volgende vier opties spreekt je het meest aan?",
+				"answers": [{
+						"answer": "Water",
+						"feedback": true
+					},
+					{
+						"answer": "Aarde",
+						"feedback": false
+					},
+					{
+						"answer": "Vuur",
+						"feedback": false
+					},
+					{
+						"answer": "Lucht",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat vind je mooier?",
+				"answers": [{
+						"answer": "Schilderijen",
+						"feedback": false
+					},
+					{
+						"answer": "3D Art",
+						"feedback": true
+					},
+					{
+						"answer": "Street art",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
